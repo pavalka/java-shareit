b274d1ae0c0a33e3ded7d98c73e466960aed2a72
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-26 20:00:53 +0300

    feat: создать интерфейс UserDao

---
+++ src/main/java/ru/practicum/shareit/user/repository/UserDao.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.user.repository;
+
+import ru.practicum.shareit.user.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserDao {
+    Optional<User> getUserById(long userId);
+    Collection<User> getAllUsers();
+    User save(User user);
+    Optional<User> updateName(long userId, String userName);
+    Optional<User> updateEmail(long userId, String userEmail);
+    Optional<User> updateNameAndEmail(long userId, String userName, String userEmail);
+    Optional<User> update(User user);
+    void delete(long userId);
+}

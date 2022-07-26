6c784ede0f91ab0ade4a5d1c42848119f27bfd47
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-26 20:02:01 +0300

    feat: создать интерфейс UserService

---
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.user.service;
+
+import ru.practicum.shareit.user.dto.UserDto;
+
+import java.util.Collection;
+
+public interface UserService {
+    UserDto getUserById(long userId);
+    UserDto createUser(UserDto userDto);
+    UserDto updateUser(UserDto userDto);
+    void deleteUser(long userId);
+    Collection<UserDto> getAllUsers();
+}

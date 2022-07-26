27fe91dc2f898dc9219da3938d3ed223a0ce816b
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-26 19:57:33 +0300

    feat: создать класс User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,14 @@
 package ru.practicum.shareit.user;
 
-/**
- * // TODO .
- */
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
 public class User {
+    private long id;
+    private String name;
+    private String email;
+    private final Set<Long> itemsList = new HashSet<>();
 }

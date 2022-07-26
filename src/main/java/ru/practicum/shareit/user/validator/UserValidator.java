271c9c1dfbe9e6871da6bc0d47a67e3689663fd8
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-26 20:02:32 +0300

    feat: создать класс UserValidator

---
+++ src/main/java/ru/practicum/shareit/user/validator/UserValidator.java
@@ -0,0 +1,14 @@
+package ru.practicum.shareit.user.validator;
+
+public class UserValidator {
+    public static boolean isUserEmailNotValid(String email) {
+        return email == null || email.isBlank() || !email.contains("@") || !email.contains(".") || email.contains(" ")
+                || email.startsWith("@") || email.endsWith("@");
+    }
+
+    public static boolean isUserNameNotValid(String name) {
+        return name == null || name.isBlank();
+    }
+
+    private UserValidator() {}
+}

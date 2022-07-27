a85cd3b2f811f94c261b59a5f3dd0d1e56f80ce5
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-27 20:32:08 +0300

    feat: создать класс InMemoryItemStorage

---
+++ src/main/java/ru/practicum/shareit/item/repository/InMemoryItemStorage.java
@@ -0,0 +1,70 @@
+package ru.practicum.shareit.item.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.practicum.shareit.item.Item;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+public class InMemoryItemStorage implements ItemDao {
+    private static long nextItemId = 1;
+    private final Map<Long, Item> itemStorage = new HashMap<>();
+
+    @Override
+    public Collection<Item> getAllItemsByOwnerId(long ownerId) {
+        return itemStorage.values().stream().filter(item -> item.getOwnerId() == ownerId)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    @Override
+    public Optional<Item> getItemById(long itemId) {
+        return Optional.ofNullable(itemStorage.get(itemId));
+    }
+
+    @Override
+    public Item save(Item item) {
+        if (item == null) {
+            throw new NullPointerException("item = null");
+        }
+
+        item.setId(getNextItemId());
+        itemStorage.put(item.getId(), item);
+        return item;
+    }
+
+    @Override
+    public Optional<Item> update(Item item) {
+        if (item == null) {
+            throw new NullPointerException("item = null");
+        }
+
+        var wrappedItem = getItemById(item.getId());
+
+        if (wrappedItem.isPresent()) {
+            itemStorage.put(item.getId(), item);
+            wrappedItem = Optional.of(item);
+        }
+        return wrappedItem;
+    }
+
+    @Override
+    public Collection<Item> findAvailableItemsByNameAndDescription(String text) {
+        if (text == null || text.isBlank()) {
+            return new ArrayList<>();
+        }
+        return itemStorage.values().stream().filter(
+                item -> (item.getName().toLowerCase().contains(text.toLowerCase())
+                        || item.getDescription().toLowerCase().contains(text.toLowerCase()))
+                        && item.isAvailable())
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private long getNextItemId() {
+        return nextItemId++;
+    }
+}

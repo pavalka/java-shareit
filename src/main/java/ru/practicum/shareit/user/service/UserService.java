9852771c708f4126c9676e0308373de212ac54c1
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-27 20:58:12 +0300

    style: добавить пустые строки между объявлениями методов в UserService

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserService {
     UserDto getUserById(long userId);
+
     UserDto createUser(UserDto userDto);
+
     UserDto updateUser(UserDto userDto);
+
     void deleteUser(long userId);
+
     Collection<UserDto> getAllUsers();
 }

55e49c807999c2c71d583e18539f3e26ff9946d7
Pavel Kabanov <pavels.disk@yandex.ru>
2022-07-27 20:58:54 +0300

    style: добавить пустые строки между объявлениями методов в ItemDao

--- src/main/java/ru/practicum/shareit/item/repository/ItemDao.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemDao.java
@@ -7,8 +7,12 @@ import java.util.Optional;
 
 public interface ItemDao {
     Collection<Item> getAllItemsByOwnerId(long ownerId);
+
     Optional<Item> getItemById(long itemId);
+
     Item save(Item item);
+
     Optional<Item> update(Item item);
+
     Collection<Item> findAvailableItemsByNameAndDescription(String text);
 }
